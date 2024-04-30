0d3b6bc9b5126d37dbaf6b6aa52027fac900ecbc
Maxim Vitugov <mvitugov@gmail.com>
2024-04-30 14:01:03 +0200

    3

--- Task16Final/WPFUsefullThings/DbHandler/DbHandler.cs
+++ Task16Final/WPFUsefullThings/DbHandler/DbHandler.cs
@@ -29,23 +29,40 @@ namespace WPFUsefullThings
                 query = query.Include(classOverview.CollectionProperty.Name);
                 foreach (var property in classOverview.CollectionGenericClassOverview.PropertiesOfCoreClass)
                 {
-                    
-                    //Func<IQueryable<T>, IIncludableQueryable<T, object>> func = i => i.Include(i => i.prop1).ThenInclude(t => t.innerprop1);
-                    string dynamicLambda = $"d => d.Include(k => k.{classOverview.CollectionProperty.Name}).ThenInclude(e => e.{property.Name})";
-                    var path = $"d => d.{classOverview.CollectionProperty.Name}.Select(e => e.{property.Name})";
-                    
-                    var elementType = typeof(T);
-                    var resultType = property.PropertyType;//typeof(ProjectModel);
 
-                    // Парсинг динамического лямбда-выражения
-                    var dynamicExpression = DynamicExpressionParser.ParseLambda(
-                        new ParsingConfig(), true, elementType, resultType, path);
-                    var expr = Expression.Lambda() // TODO!!!!!!!!!!!
-                    var delegat = (Func<IQueryable<T>, IIncludableQueryable<T, object>>)dynamicExpression.Compile();
-                    query = delegat(query);
+                    //Func<IQueryable<T>, IIncludableQueryable<T, object>> func = i => i.SuperInclude(i => i.prop1).ThenInclude(t => t.innerprop1);
+                    //string dynamicLambda = $"d.SuperInclude(k => k.{classOverview.CollectionProperty.Name}).ThenInclude(e => e.{property.Name})";
+                    //var path = $"d => d.{classOverview.CollectionProperty.Name}.Select(e => e.{property.Name})";
+
+                    //var elementType = typeof(IQueryable<T>); //typeof(T);
+                    //var resultType = typeof(IIncludableQueryable<T, object>);//property.PropertyType;//typeof(ProjectModel);
+
+                    //// Парсинг динамического лямбда-выражения
+                    //var dynamicExpression = DynamicExpressionParser.ParseLambda(
+                    //    new ParsingConfig(), true, elementType, resultType, dynamicLambda);
+
+                    //Expression<Func<IQueryable<T>, string>> expression = (d) => path;
+                    //var func = expression.Compile();
+                    //func(query).Dump()
+                    ////var expr = Expression.Lambda() // TODO!!!!!!!!!!!
+                    //var delegat = (Func<IQueryable<T>, IIncludableQueryable<T, object>>)dynamicExpression.Compile();
+                    //query = delegat(query);
 
                     //(Expression<Func<T, ProjectModel>>)dynamicExpression;
-                    //query = query.Include($"d => d.{classOverview.CollectionProperty.Name}").ThenInclude($"e => e.{ property.Name})");
+
+                    var expressionParameter = Expression.Parameter(typeof(T));
+                    var expression = Expression.Lambda(
+                        Expression.Property(expressionParameter, classOverview.CollectionProperty.Name),
+                        expressionParameter);
+                    var typee = expression.Type;
+
+                    //query = query.Include((Expression<Func<T, ObservableCollection<ProjectModel>>>)expression).ThenInclude<T, ProjectModel, ProjectModel>(Expression<Func<ProjectModel,ProjectModel>> expr);
+                    query = query.SuperInclude($"{classOverview.CollectionProperty.Name}.{property.Name}");
+                    //query = query.Include(e => e.Id).ThenInclude();
+                    //Func<T, ObservableCollection<ProjectModel>> func = d => (ObservableCollection<ProjectModel>)typeof(T).GetProperty(classOverview.CollectionProperty.Name).GetValue(d);
+                    //query = query
+                    //    .SuperInclude(k => (ObservableCollection<ProjectModel>)typeof(T).GetProperty(classOverview.CollectionProperty.Name).GetValue(k))
+                    //    .ThenInclude(e => e.GetType().GetProperty(property.Name).GetValue(e));
                     //var formatExpr = Expression.Call(
                     //formatStaticMethod,
                     //Expression.Constant(parsed.format, typeof(string)),
@@ -82,19 +99,19 @@ namespace WPFUsefullThings
 
             foreach (var property in classOverview.PropertiesOfCoreClass)
             {
-                query = query.Include(property.Name);
+                query = query.SuperInclude(property.Name);
             }
             if (classOverview.HaveCollection)
             {
-                query = query.Include(classOverview.CollectionProperty.Name);
+                query = query.SuperInclude(classOverview.CollectionProperty.Name);
                 foreach (var property in classOverview.CollectionGenericClassOverview.PropertiesOfCoreClass)
                 {
                     //var k = DynamicExpressionParser.ParseLambda()
                     //var path = $"d => d.{classOverview.CollectionProperty.Name}.Select(e => e.{property.Name})";
-                    //query = query.Include(e => EF.Property<IEnumerable<ProjectModel>>(e, classOverview.CollectionProperty.Name)
+                    //query = query.SuperInclude(e => EF.Property<IEnumerable<ProjectModel>>(e, classOverview.CollectionProperty.Name)
                     //    .Select(x => EF.Property<ProjectModel>(x, property.Name)));
 
-                    //query.Include(classOverview.CollectionProperty.Name).ThenInclude(property.Name);
+                    //query.SuperInclude(classOverview.CollectionProperty.Name).ThenInclude(property.Name);
                     // Строка лямбда-выражения с динамическими именами свойств
                     //string dynamicLambda = $"d => d.{classOverview.CollectionProperty.Name}.Select(e => e.{property.Name})";
 
@@ -106,7 +123,7 @@ namespace WPFUsefullThings
                     //var dynamicExpression = DynamicExpressionParser.ParseLambda(
                     //    new ParsingConfig(), true, elementType, resultType, dynamicLambda);
                     //var expression = dynamicExpression as Expression<Func<ProjectModel, ProjectModel>>;
-                    //query = query.Include((Expression<Func<ProjectModel,ProjectModel>>)dynamicExpression);
+                    //query = query.SuperInclude((Expression<Func<ProjectModel,ProjectModel>>)dynamicExpression);
 
                     // Дополнительно: использование полученного выражения (нужен контекст использования)
                     // Пример использования был бы с IQueryable<DataItem> или подобным
@@ -121,7 +138,7 @@ namespace WPFUsefullThings
 
 
         //}
-        //Func<IQueryable<T>, IIncludableQueryable<T, object>> func = i => i.Include(i => i.prop1).ThenInclude(t => t.innerprop1);
+        //Func<IQueryable<T>, IIncludableQueryable<T, object>> func = i => i.SuperInclude(i => i.prop1).ThenInclude(t => t.innerprop1);
         //string dynamicLambda = $"d => d.{classOverview.CollectionProperty.Name}.Select(e => e.{property.Name})";
 
         //var elementType = typeof(T);
@@ -131,5 +148,7 @@ namespace WPFUsefullThings
         //var dynamicExpression = DynamicExpressionParser.ParseLambda(
         //    new ParsingConfig(), true, elementType, resultType, dynamicLambda);
         //var delegat = (Func<IQueryable<T>, IIncludableQueryable<T, object>>)dynamicExpression.Compile();
+
+
     }
 }
--- Task16Final/WPFUsefullThings/Extensions.cs
+++ Task16Final/WPFUsefullThings/Extensions.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,23 +32,90 @@ namespace WPFUsefullThings
             return result;
         }
 
-        //public static Dictionary<string, ObservableCollection<KeyValuePair<string, ProjectModel>>>
-        //    GetDictionariesOfRelatedProperties(this DbContext context, Type type)
-        //{
-        //    var dic = new Dictionary<string, ObservableCollection<KeyValuePair<string, ProjectModel>>>();
-        //    var properties = type.GetPropertiesOfType(typeof(ProjectModel));
-        //    foreach (var property in properties)
-        //    {
-        //        using (context)
-        //        {
-        //            var set = context.GetDeepData(property.PropertyType);
-        //            var keyValuePairSet = set.Select(obj => new KeyValuePair<string, ProjectModel>(obj.ToViewString() ?? "", obj));
-        //            var collection = new ObservableCollection<KeyValuePair<string, ProjectModel>>(keyValuePairSet);
-        //            dic[property.Name] = collection;
-        //        }
-        //    }
-        //    return dic;
-        //}
+
+        private static MethodInfo _include = typeof(EntityFrameworkQueryableExtensions)
+            .GetTypeInfo()
+            .GetDeclaredMethods("Include")
+            .Single((MethodInfo mi) => mi.GetGenericArguments().Length == 2 && mi
+                .GetParameters()
+                .Any((ParameterInfo pi) => pi.Name == "navigationPropertyPath" && pi.ParameterType != typeof(string)));
+
+        private static MethodInfo _thenIncludeReference = typeof(EntityFrameworkQueryableExtensions)
+            .GetMethods()
+            .Where(m => m.Name == "ThenInclude")
+            .Single(m => m.Name == "ThenInclude" &&
+                         m.GetParameters()
+                          .Single(p => p.Name == "source")
+                          .ParameterType
+                          .GetGenericArguments()[1].Name != typeof(ICollection<>).Name);
+
+        private static MethodInfo _thenIncludeCollection = typeof(EntityFrameworkQueryableExtensions)
+            .GetMethods()
+            .Where(m => m.Name == "ThenInclude")
+            .Single(m => m.Name == "ThenInclude" &&
+                         m.GetParameters()
+                          .Single(p => p.Name == "source")
+                          .ParameterType
+                          .GetGenericArguments()[1].Name == typeof(ICollection<>).Name);
+
+        public static IQueryable<T> SuperInclude<T>(this IQueryable<T> query, string include)
+        {
+            return query.SuperInclude(include.Split('.'));
+        }
+
+        public static IQueryable<T> SuperInclude<T>(this IQueryable<T> query, params string[] include)
+        {
+            var currentType = query.ElementType;
+            var previousNavWasCollection = false;
+
+            for (int i = 0; i < include.Length; i++)
+            {
+                var navigationName = include[i];
+                var navigationProperty = currentType.GetProperty(navigationName);
+                if (navigationProperty == null)
+                {
+                    throw new ArgumentException($"'{navigationName}' is not a valid property of '{currentType}'");
+                }
+
+                MethodInfo includeMethod;
+                if (i == 0)
+                {
+                    includeMethod = _include.MakeGenericMethod([query.ElementType, navigationProperty.PropertyType]);
+                }
+                else
+                {
+                    if (previousNavWasCollection)
+                    {
+                        includeMethod = _thenIncludeCollection.MakeGenericMethod(query.ElementType, currentType, navigationProperty.PropertyType);
+                    }
+                    else
+                    {
+                        includeMethod = _thenIncludeReference.MakeGenericMethod(query.ElementType, currentType, navigationProperty.PropertyType);
+                    }
+                }
+
+                var expressionParameter = Expression.Parameter(currentType);
+                var expression = Expression.Lambda(
+                    Expression.Property(expressionParameter, navigationName),
+                    expressionParameter);
+
+                query = (IQueryable<T>)includeMethod.Invoke(null, new object[] { query, expression });
+
+                if (navigationProperty.PropertyType.GetInterfaces().Any(x => x.Name == typeof(ICollection<>).Name))
+                {
+                    previousNavWasCollection = true;
+                    currentType = navigationProperty.PropertyType.GetGenericArguments().Single();
+                }
+                else
+                {
+                    previousNavWasCollection = false;
+                    currentType = navigationProperty.PropertyType;
+                }
+            }
+
+            return query;
+        }
+
 
 
     }
